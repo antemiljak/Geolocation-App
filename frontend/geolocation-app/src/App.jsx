1bd88e49711e961ec124dc2f77acaf481500c31d
Ante Miljak <amilja01@fesb.hr>
2025-01-23 16:33:14 +0100

    .

--- frontend/geolocation-app/src/App.jsx
+++ frontend/geolocation-app/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
-import Signup from "./pages/Signup";
+import SignUp from "./pages/Signup";
 import Login from "./pages/Login";
 import AllRoutesPage from "./pages/AllRoutesPage";
 import Stats from "./pages/Stats";
@@ -13,7 +13,7 @@ const routes = (
       <Route path="/" exact element={<LandingPage />} />
       <Route path="/home" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
-      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/signup" exact element={<SignUp />} />
       <Route path="/allroutespage" exact element={<AllRoutesPage />} />
       <Route path="/stats" exact element={<Stats />} />
     </Routes>
