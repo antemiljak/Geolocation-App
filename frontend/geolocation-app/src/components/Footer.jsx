b41c8f67a011fcc75cc11ba060b42e5dffe354e1
Ante Miljak <amilja01@fesb.hr>
2025-01-27 19:48:02 +0100

    design

--- frontend/geolocation-app/src/components/Footer.jsx
+++ frontend/geolocation-app/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer class="bg-zinc-900">
       <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="flex items-center justify-between">
+        <div class="flex items-center justify-between gap-2">
           <img src={Logo} alt="logo" className="w-48" />
 
           <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-slate-300 sm:mb-0 ">
@@ -16,7 +16,7 @@ const Footer = () => {
           </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <span class="block text-sm text-green-300 text-center">
+        <span class="block text-sm text-slate-300 text-center">
           © 2025{" "}
           <a href="https://github.com/antemiljak/" class="hover:underline">
             Ante Miljak
